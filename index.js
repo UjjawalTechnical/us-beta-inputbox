11df2c5d5cdda529bc2aac4833407f00b612b77b
Ujjawal Technical <ujjawal.technical2518@gmail.com>
2026-07-23 11:57:12 +0530

    input changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useImperativeHandle, forwardRef, useId } from "react";
 import { Eye, EyeOff, X, Search, Mail, Lock, User, DollarSign, Hash } from "lucide-react"; 
-import "./src/styles.css";
 
 /* ----------- Utils Start ---------- */
 export const convertToNumber = (value) => {
